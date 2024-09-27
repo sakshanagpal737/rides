d938c8042a757e48f9a369560b100b52e38770ef
saksha <sakshanagpal@gmail.com>
2024-09-27 18:14:25 -0400

    Initial Commit

---
+++ Rides/RidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RidesApp.swift
+//  Rides
+//
+//  Created by Saksha Nagpal on 2024-09-27.
+//
+
+import SwiftUI
+
+@main
+struct RidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
