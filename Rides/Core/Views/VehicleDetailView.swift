72ed9d550f368899fbc9a198bb09aec67c76d244
saksha <sakshanagpal@gmail.com>
2024-09-28 02:31:00 -0400

    with change in network call and added details view

---
+++ Rides/Core/Views/VehicleDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  VehicleDetailView.swift
+//  Rides
+//
+//  Created by Saksha Nagpal on 2024-09-28.
+//
+
+import SwiftUI
+
+struct VehicleDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VehicleDetailView()
+}
---
+++ Rides/Extentions/UIApplication.swift
@@ -0,0 +1,8 @@
+//
+//  UIApplication.swift
+//  Rides
+//
+//  Created by Saksha Nagpal on 2024-09-28.
+//
+
+import Foundation
