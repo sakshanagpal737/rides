86d74577dd9fdbfcb7ad06655256ca657f0e0d86
saksha <sakshanagpal@gmail.com>
2024-10-02 15:03:52 -0400

    with launch screen and app icon

---
+++ Rides/Core/Views/Launch/LaunchView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchView.swift
+//  Rides
+//
+//  Created by Saksha Nagpal on 2024-10-02.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchView()
+}
